23857324e0767748496e0fbbe54a3bf357a8998b
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2024-10-31 22:29:44 +0000

    ensure git and add-apt-repository is installed
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -19,9 +19,13 @@ sudo systemctl disable --now ssh
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 
-sudo add-apt-repository --yes ppa:ansible/ansible
 sudo apt-get --assume-yes update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install --no-install-recommends git software-properties-common
 
+sudo add-apt-repository --yes ppa:ansible/ansible
+
+sudo apt-get --assume-yes update
 sudo apt-get --assume-yes --no-install-recommends install ansible netfilter-persistent iptables-persistent
 
 cd /tmp || exit 1
